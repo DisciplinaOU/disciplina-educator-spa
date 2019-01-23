900788f002ec297db13471ebd436049f1a341761
Kubas <dru.kubasov@gmail.com>
2019-01-23 18:19:20 +0300

    general routing / authguard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+import { Route, Link, Switch, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
+
+const AuthGuard = {
+  checkAuth: async () => Promise.resolve(() => true)
+};
+
+const withAuth = (WrappedComponent: Component) => {
+  return class PrivateContainer extends Component {
+    state = {
+      isLoading: true,
+      isAuthenticated: false
+    };
+    
+    async componentDidMount() {
+      try {
+        await AuthGuard.checkAuth();
+        this.setState({ isLoading: false, isAuthenticated: true });
+      } catch (e) {
+        this.setState({ isLoading: false, isAuthenticated: false });
+      }
+    }
+    
+    render() {
+      const { isAuthenticated, isLoading } = this.state;
+      if (isLoading) return <h5>Loading...</h5>;
+      if (!isAuthenticated) return <Redirect to="/auth" />;
+      return <WrappedComponent {...this.props} />;
+    }
+  }
+};
+
+const Auth = () => <h2>Welcome Auth...</h2>;
+const Faircv = () => (
+  <Switch>
+    <Route exact path="/faircv" render={() => <h2>Welcome fair list...</h2>} />
+    <Route exact path="/faircv/create" render={() => <h2>Welcome fair create...</h2>} />
+  </Switch>
+);
 
 class App extends Component {
   render() {
@@ -20,6 +58,13 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/faircv" component={withAuth(Faircv)} />
+        </Switch>
+        <Link to="/auth">auth</Link>
+        <Link to="/faircv">faircv list</Link>
+        <Link to="/faircv/create">create faircv</Link>
       </div>
     );
   }
