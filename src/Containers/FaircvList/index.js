32c5e70dac47598f023bf654d7fb796e083e0e71
Kubas <dru.kubasov@gmail.com>
2019-02-12 11:35:43 +0300

    history callbacks and wortked pagination

--- src/Containers/FaircvList/index.js
+++ src/Containers/FaircvList/index.js
@@ -16,44 +16,51 @@ type FairCVListDataType = {
 
 type FaircvListState = {
   data: Array<FairCVListDataType>,
-  totalCount: number,
   pages: number,
   currentPage: number
 };
 
-class FaircvList extends PureComponent<{}, FaircvListState> {
+type FaircvListProps = {
+  history: any
+};
+
+class FaircvList extends PureComponent<FaircvListProps, FaircvListState> {
   api: IFaircvService = FaircvService;
 
   state: FaircvListState = {
     data: [],
-    totalCount: 0,
     pages: 0,
     currentPage: -1
   };
 
   async componentDidMount() {
     const data = await this.api.getList();
+    const { items, count } = data.data;
     this.setState({
-      data: data.data.items,
-      totalCount: data.data.count,
-      pages: Math.round(data.data.count / 10),
+      data: items,
+      pages: Math.round(count / 10),
       currentPage: 1
     });
   }
-  
+
+  createFairHandler = () => {
+    const { history } = this.props;
+    history.push("/faircv/create");
+  };
+
   goToPage = (p: number) => this.setState({ currentPage: p });
-  
+
   goFwd = () => {
     this.setState(s => {
       if (s.currentPage === s.pages) return null;
-      return { currentPage: s.currentPage++ };
+      return { currentPage: s.currentPage + 1 };
     });
   };
-  
+
   goBcwd = () => {
     this.setState(s => {
       if (s.currentPage === 1) return null;
-      return { currentPage: s.currentPage-- };
+      return { currentPage: s.currentPage - 1 };
     });
   };
 
@@ -61,6 +68,7 @@ class FaircvList extends PureComponent<{}, FaircvListState> {
     const { data, pages, currentPage } = this.state;
     const isDesktop = document.documentElement && document.documentElement.clientWidth >= 768;
     const searchPlaceholder = isDesktop ? "Введите имя студента или номер диплома" : "Поиск";
+    const normalizedArray = [...data].splice((currentPage - 1) * 10, 10);
     return (
       <div className="faircv-list container">
         <div className="faircv-list__title">
@@ -71,10 +79,10 @@ class FaircvList extends PureComponent<{}, FaircvListState> {
             modHeight="height-big"
             modStyle="filled"
             modColor="color-main"
-            callback={() => {}}
+            callback={this.createFairHandler}
           />
         </div>
-        {data.length ? (
+        {normalizedArray.length ? (
           <>
             <form className="faircv-list__search">
               <RegularInput
@@ -94,7 +102,7 @@ class FaircvList extends PureComponent<{}, FaircvListState> {
               />
             </form>
             <ul className="list">
-              {data.map((item: Certificate) => (
+              {normalizedArray.map((item: Certificate) => (
                 <li className="list__item" key={item.id}>
                   <div className="list__item-content">
                     <div className="list__item-name">{item.meta.studentName}</div>
