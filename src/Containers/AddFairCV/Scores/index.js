c9ee17a9a1c55936109745c0ec0dd2c7ff1be441
Kubas <dru.kubasov@gmail.com>
2019-02-20 17:00:26 +0300

    fix score edit by index

--- src/Containers/AddFairCV/Scores/index.js
+++ src/Containers/AddFairCV/Scores/index.js
@@ -43,7 +43,7 @@ export class Scores extends PureComponent<ScoresProps, ScoresState> {
     const { dispatchScores } = this.props;
     const { data } = this.state;
     let updatedData;
-    if (scoreIndex) {
+    if (scoreIndex || scoreIndex === 0) {
       data[scoreIndex] = scoreItem;
       updatedData = data;
     } else {
