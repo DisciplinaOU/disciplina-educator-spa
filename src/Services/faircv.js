f0fa7dc8c2692727f89474f3da6144958883b4f4
Kubas <dru.kubasov@gmail.com>
2019-02-21 12:45:55 +0300

    added base64 url encoding to download pdf

--- src/Services/faircv.js
+++ src/Services/faircv.js
@@ -1,4 +1,5 @@
 import axios, { AxiosStatic, AxiosInstance } from "axios";
+import base64url from "base64url";
 import type { Certificate, FaircvListResponse, FaircvQuery, IFaircvService, NewCertificate } from "./types";
 
 const API_URL = process.env.REACT_APP_EDUCATOR;
@@ -29,15 +30,38 @@ class FaircvService implements IFaircvService {
   }
 
   getList(filter?: FaircvQuery): Promise<FaircvListResponse> {
-    return this.httpService.get(`${BASE_URL}${this._serializefilter(filter)}`, { headers: { 'Authorization': 'Bearer eyJhbGciOiJFZERTQSIsImp3ayI6eyJjcnYiOiJFZDI1NTE5IiwieCI6IlI2Qlo3OGx5bHRVdEdBT012bFR4dG9ZeXRYS1E0OXQxSUNxb1EwQWtLWVUiLCJrdHkiOiJPS1AifX0.eyJwYXRoIjoiL2FwaS9lZHVjYXRvci92MS9jZXJ0aWZpY2F0ZXMiLCJ0aW1lIjoiMjAyNS0wMS0wMVQwMDowMDowMFoifQ.ifQScfwBi8EQunBpHkLg5eoDA2YYWvKcg1UepHisSGa4kh1yP0e4gbn-ZZufAmECyk4_x35uOmaJSn9mI92_Dw' } });
+    return this.httpService.get(`${BASE_URL}${this._serializefilter(filter)}`, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJFZERTQSIsImp3ayI6eyJjcnYiOiJFZDI1NTE5IiwieCI6IlI2Qlo3OGx5bHRVdEdBT012bFR4dG9ZeXRYS1E0OXQxSUNxb1EwQWtLWVUiLCJrdHkiOiJPS1AifX0.eyJwYXRoIjoiL2FwaS9lZHVjYXRvci92MS9jZXJ0aWZpY2F0ZXMiLCJ0aW1lIjoiMjAyNS0wMS0wMVQwMDowMDowMFoifQ.ifQScfwBi8EQunBpHkLg5eoDA2YYWvKcg1UepHisSGa4kh1yP0e4gbn-ZZufAmECyk4_x35uOmaJSn9mI92_Dw"
+      }
+    });
   }
 
   get(id: number): Promise<File> {
-    return this.httpService.get(`${DOWNLOAD}/${id}`, { headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJFZERTQSIsImp3ayI6eyJjcnYiOiJFZDI1NTE5IiwieCI6IlI2Qlo3OGx5bHRVdEdBT012bFR4dG9ZeXRYS1E0OXQxSUNxb1EwQWtLWVUiLCJrdHkiOiJPS1AifX0.eyJwYXRoIjoiL2FwaS9lZHVjYXRvci92MS9jZXJ0aWZpY2F0ZSIsInRpbWUiOiIyMDI1LTAxLTAxVDAwOjAwOjAwWiJ9.7vja7dC0q2HcPHthSoDCwu4rW9Ioto3AU5R2eIKWuFm3HjkhGKNBjn06CXSG2LCj-Mo_kAD_gTS0od_GT5O8Bg' } });
+    const certId = this._makeCertId(id);
+    return this.httpService.get(`${DOWNLOAD}${this._serializefilter({ id: certId })}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJFZERTQSIsImp3ayI6eyJjcnYiOiJFZDI1NTE5IiwieCI6IlI2Qlo3OGx5bHRVdEdBT012bFR4dG9ZeXRYS1E0OXQxSUNxb1EwQWtLWVUiLCJrdHkiOiJPS1AifX0.eyJwYXRoIjoiL2FwaS9lZHVjYXRvci92MS9jZXJ0aWZpY2F0ZSIsInRpbWUiOiIyMDI1LTAxLTAxVDAwOjAwOjAwWiJ9.7vja7dC0q2HcPHthSoDCwu4rW9Ioto3AU5R2eIKWuFm3HjkhGKNBjn06CXSG2LCj-Mo_kAD_gTS0od_GT5O8Bg"
+      }
+    });
   }
 
   create(cert: NewCertificate): Promise<Certificate> {
     return this.httpService.post(BASE_URL, cert);
+  }
+
+  _makeCertId(hash: string): string {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const baseEducatorData = token.split(".")[1];
+      const decodedEducatorData = base64url.decode(baseEducatorData);
+      const educator = JSON.parse(decodedEducatorData.toString());
+      return base64url.encode(`${educator.data.id}:${hash}`);
+    }
+    return "";
   }
 
   _serializefilter(filter: {}): string {
