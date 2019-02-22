d238c6abc2696a597ce6f8e820da2ab0c7d75b1c
Kubas <dru.kubasov@gmail.com>
2019-02-22 14:58:50 +0300

    fixed gitlab envs

--- src/Services/faircv.js
+++ src/Services/faircv.js
@@ -21,7 +21,7 @@ class FaircvService implements IFaircvService {
     this.httpService.interceptors.request.use(
       config => {
         const token = localStorage.getItem("token");
-        if (token) config.headers.authorization = token;
+        if (token) config.headers.authorization = `Bearer ${token}`;
         return config;
       },
       error => Promise.reject(error)
