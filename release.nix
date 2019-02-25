5819f73db1a23168af404ad5964f09b57d28b16e
Dmitry Mukhutdinov <flyingleafe@gmail.com>
2019-02-25 17:29:56 +0300

    Add Sass binary path

--- release.nix
+++ release.nix
@@ -4,11 +4,17 @@
 , parallel
 , brotli
 , constGitIgnore
+, fetchurl
 }:
 
 buildYarnPackage {
   REACT_APP_AAA = aaaUrl;
   REACT_APP_EDUCATOR = educatorUrl;
+  SASS_BINARY_PATH = fetchurl {
+    url = https://github.com/sass/node-sass/releases/download/v4.11.0/linux-x64-64_binding.node;
+    sha256 = "cef583e91b02a585458d28c12429e442026c4083ef31900024445912080d8936";
+  };
+
   src = constGitIgnore "educator-spa" ./. [];
 
   buildInputs = [ parallel brotli ];
