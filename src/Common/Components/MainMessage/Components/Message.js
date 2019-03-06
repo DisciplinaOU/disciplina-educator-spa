30d0deb7fbfe206ddd93dcd04b87b564c4f8c34c
Kubas <dru.kubasov@gmail.com>
2019-03-06 16:41:56 +0300

    make message pure memo

--- src/Common/Components/MainMessage/Components/Message.js
+++ src/Common/Components/MainMessage/Components/Message.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 import Button from "../../Button";
 
 type MessageProps = {
@@ -9,37 +9,34 @@ type MessageProps = {
   buttonText?: string
 };
 
-export class Message extends PureComponent<MessageProps> {
-  static defaultProps = {
-    text: "",
-    buttonText: ""
-  };
-
-  goToAuth = () => {
+const Message = (props: MessageProps) => {
+  const goToAuth = () => {
     window.history.replaceState(null, null, window.location.pathname);
     window.location.pathname = "/auth";
   };
+  const { img, title, text, buttonText = "" } = props;
+  return (
+    <div className="mail_confirmed">
+      <img className="mail_confirmed__img" src={img} alt="" />
+      <h3 className="mail_confirmed__title">{title}</h3>
+      {text && text.length ? <p className="mail_confirmed__text">{text}</p> : null}
+      {buttonText && buttonText.length ? (
+        <Button
+          text={buttonText}
+          modWidth="width-auto"
+          modHeight="height-big"
+          modStyle="filled"
+          modColor="color-main"
+          callback={goToAuth}
+        />
+      ) : null}
+    </div>
+  );
+};
 
-  render() {
-    const { img, title, text, buttonText = "" } = this.props;
-    return (
-      <div className="mail_confirmed">
-        <img className="mail_confirmed__img" src={img} alt="" />
-        <h3 className="mail_confirmed__title">{title}</h3>
-        {text && text.length ? <p className="mail_confirmed__text">{text}</p> : null}
-        {buttonText && buttonText.length ? (
-          <Button
-            text={buttonText}
-            modWidth="width-auto"
-            modHeight="height-big"
-            modStyle="filled"
-            modColor="color-main"
-            callback={this.goToAuth}
-          />
-        ) : null}
-      </div>
-    );
-  }
-}
+Message.defaultProps = {
+  text: "",
+  buttonText: ""
+};
 
-export default Message;
+export default memo<MessageProps>(Message);
