00cce2682d73d4a59cebac41b65e3118d8584f4c
Kubas <dru.kubasov@gmail.com>
2019-03-06 16:30:43 +0300

    make auth container pure memo

--- src/Containers/Auth/index.js
+++ src/Containers/Auth/index.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from "react";
+import React, { memo } from "react";
 import AuthForm from "../AuthForm";
 import MainMessage from "../../Common/Components/MainMessage";
 import type { Educator } from "../../Services/types";
@@ -10,16 +10,16 @@ type AuthContainerProps = {
   location: any
 };
 
-export default class AuthContainer extends PureComponent<AuthContainerProps, {}> {
-  render() {
-    const { history, location, user } = this.props;
+const AuthContainer = (props: AuthContainerProps) => {
+  const { history, location, user } = props;
 
-    if (user.id && !user.confirmedAt) {
-      return <MainMessage type="CHECK_EMAIL" />;
-    }
-    if (user.id && !user.confirmedByOrganization) {
-      return <MainMessage type="NOT_CONFIRMED" />;
-    }
-    return <AuthForm history={history} location={location} />;
+  if (user.id && !user.confirmedAt) {
+    return <MainMessage type="CHECK_EMAIL" />;
   }
-}
+  if (user.id && !user.confirmedByOrganization) {
+    return <MainMessage type="NOT_CONFIRMED" />;
+  }
+  return <AuthForm history={history} location={location} />;
+};
+
+export default memo<AuthContainerProps>(AuthContainer);
