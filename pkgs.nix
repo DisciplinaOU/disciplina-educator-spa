ce38de26530824ff95b3520910b5a57aa3565817
hbb228 <hbb228@protonmail.com>
2022-04-11 21:27:24 +0100

    Update Nix build files

--- pkgs.nix
+++ pkgs.nix
@@ -1,6 +1,20 @@
-import (fetchGit {
-  url = https://github.com/DisciplinaOU/nixpkgs;
-  ref = "20210415.2118";
-  rev = "13454545f1453ee688b394c63045fbc5cc99fcc5";
-})
+# (import (fetchGit {
+#   url = https://github.com/DisciplinaOU/serokell-closure;
+#   rev = "7b26d2aa7b99b11201679b51d80a963f9aac4847";
+#   ref = "20210415.2126";
+let
+  sources = import ./nix/sources.nix;
+  haskellNix = import sources.haskellNix {};
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
 
+  bp = pkgs.callPackage (pkgs.fetchFromGitHub {
+    owner = "serokell";
+    repo = "nix-npm-buildpackage";
+    rev = "881f4bfa68e33cd0fb69e6b739fb92f8d0bbe37e";
+    sha256 = "0wqmxijinm9mjcm3n5id13phmapqcxcrxwi106wvg0krca3ki58x";
+  }) {};
+
+in pkgs.extend(final: previous: with previous; rec {
+  buildYarnPackage = bp.buildYarnPackage;
+  constGitIgnore = _: path: _: path;
+})
--- release.nix
+++ release.nix
@@ -3,6 +3,7 @@
 , buildYarnPackage
 , parallel
 , brotli
+, python
 , constGitIgnore
 , fetchurl
 }:
@@ -11,13 +12,13 @@ buildYarnPackage {
   REACT_APP_AAA = aaaUrl;
   REACT_APP_EDUCATOR = educatorUrl;
   SASS_BINARY_PATH = fetchurl {
-    url = https://github.com/sass/node-sass/releases/download/v4.11.0/linux-x64-64_binding.node;
-    sha256 = "cef583e91b02a585458d28c12429e442026c4083ef31900024445912080d8936";
+    url = https://github.com/sass/node-sass/releases/download/v7.0.1/linux-x64-93_binding.node;
+    sha256 = "1rzrzyy5sla708qypr7s3kqwc3g0dhc99bjvxwf3qq4dfygags7x";
   };
 
   src = constGitIgnore "educator-spa" ./. [];
 
-  buildInputs = [ parallel brotli ];
+  buildInputs = [ parallel brotli python ];
 
   postBuild = ''
     find build/ -type f \
